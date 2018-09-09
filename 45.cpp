8ad9e3cf9f72bae4c2b3a83d0972b00bfb1992f1
JoschkaZ <Joschka.Ziesche15@imperial.ac.uk>
2018-09-09 17:10:06 +0200

    .

---
+++ 45.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+  int age = 21; // C-like initialization
+  int age2 (21); // Constructor initialization
+  int age3 {21}; // C++11 list initialization syntax
+
+  int age4;
+  cout << age4;
+
+  return 0;
+}
