19caafef6b2ca13e484332488d9557711360d9e9
JoschkaZ <Joschka.Ziesche15@imperial.ac.uk>
2018-09-09 16:11:05 +0200

    .

--- 4-27.cpp
+++ 4-27.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-int main(){
-
+int main()
+{
   int favorite_number;
-
   std::cout << "Enter your favourite number between 1 and 100:";
   std::cin >> favorite_number;
   std::cout << "Amazing..." << std::endl;
-  return 0;
+  std::cout << favorite_number << " is a great number.." << std::endl;
+  return 4;
 }
+
+//std: standard name space
